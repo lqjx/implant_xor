533cedad54e18bf2292a466aa6e056abc6669888
KOZY <kozy@kozy.kozy>
2024-10-09 21:54:47 +0200

    Added encrypted payload :D

--- xorencrypt.py
+++ xorencrypt.py
@@ -8,29 +8,26 @@ import sys
 KEY = "mysecretkeee"
 
 def xor(data, key):
-	
-	key = str(key)
-	l = len(key)
-	output_str = ""
+    key = str(key)
+    l = len(key)
+    output_bytes = bytearray()
 
-	for i in range(len(data)):
-		current = data[i]
-		current_key = key[i % len(key)]
-		output_str += chr(ord(current) ^ ord(current_key))
-	
-	return output_str
+    for i in range(len(data)):
+        current = data[i]  # data is now a byte, no need for ord()
+        current_key = ord(key[i % len(key)])  # ord() is applied only to the key
+        output_bytes.append(current ^ current_key)
+
+    return output_bytes
 
 def printCiphertext(ciphertext):
-	print('{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
-
-
+    print('{ 0x' + ', 0x'.join(hex(x)[2:] for x in ciphertext) + ' };')
 
 try:
-    plaintext = open(sys.argv[1], "rb").read()
+    plaintext = open(sys.argv[1], "rb").read()  # read as binary data
 except:
     print("File argument needed! %s <raw payload file>" % sys.argv[0])
     sys.exit()
 
-
 ciphertext = xor(plaintext, KEY)
-print('{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
+printCiphertext(ciphertext)
+
